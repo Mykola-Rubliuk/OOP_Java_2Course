7eaae5edd0a5a2445371fe47e7e8bef3ec312c6b
StudentKolyaRubliuk( <morry.GitHub@gmail.com>
2025-12-01 16:07:42 +0000

    Initial commit

--- src/labsheet16/exercise1/TestComputer.java
+++ src/labsheet16/exercise1/TestComputer.java
@@ -6,29 +6,47 @@ package labsheet16.exercise1;
 public class TestComputer {
     public static void main(String[] args) {
 
-        double totalPrice=0;
+        double totalPrice = 0;
         Computer highestSpeedComputer;
+        String priceOver500 = "";
+        String typeTablet = "";
 
         Computer c1 = new Computer();
-        Computer c2 = new Computer("Dell","Laptop",3.25,16,550.99);
-        Computer c3 = new Computer("Apple","Tablet",3.65,8,500.25);
-        Computer c4 = new Computer("Sun","Server",4.25,32,1768.45);
+        Computer c2 = new Computer("Dell", "Laptop", 3.25, 16, 550.99);
+        Computer c3 = new Computer("Apple", "Tablet", 3.65, 8, 500.25);
+        Computer c4 = new Computer("Sun", "Server", 4.25, 32, 1768.45);
+        Computer c5 = new Computer("Lenovo", "Desktop", 2.86, 14, 457.34);
 
-        Computer[] allComputers = {c1,c2,c3,c4};
+        Computer[] allComputers = {c1, c2, c3, c4, c5};
 
         highestSpeedComputer = allComputers[0];
 
-        System.out.println("\n\nDetails of all Computers in the array\n");
-        for (Computer c:allComputers) {
-            totalPrice += c.getPrice();
+        System.out.println("Some additional information\n\n");
+        for (Computer computer : allComputers) {
+            totalPrice += computer.getPrice();
+            if (computer.getPrice() >= 500) {
+                priceOver500 += computer + "\n";
+            }
+            if (highestSpeedComputer.getSpeed() <= computer.getSpeed()) {
+                highestSpeedComputer = computer;
+            }
+            if (computer.getType().equals("Tablet")) {
+                typeTablet += computer + "\n";
+            }
+        }
+        System.out.printf("Average computer price is: $%.2f\n", totalPrice / allComputers.length);
+        System.out.println("Details of highest speed computer: " + highestSpeedComputer);
+        System.out.println("List of computers whose price exceeds $500:\n\n" + priceOver500);
+        System.out.println("\nList of computers that are tablets:\n" + typeTablet);
+        System.out.println("\nUpdated details of all Computers in the array\n\n");
 
-            if(c.getSpeed()>highestSpeedComputer.getSpeed())
-                highestSpeedComputer = c;
-
-            System.out.println(c);
+        for (Computer computer : allComputers) {
+            if (computer.getManufacturer().equals("Lenovo")) {
+                computer.setManufacturer("Dell");
+            }
+            System.out.println(computer);
         }
 
-        System.out.println("\n\nSome additional information\n\nAverage computer price is: €" + String.format("%.2f",totalPrice/allComputers.length)+
-                           "\nDetails of highest speed computer: " + highestSpeedComputer);
+
     }
 }
